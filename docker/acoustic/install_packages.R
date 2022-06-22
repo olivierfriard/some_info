074428ce0efdd27d12b7987d03c30fb18b4eadf8
Olivier Friard <olivier.friard@unito.it>
2022-06-22 09:59:46 +0200

    some work

---
+++ docker/acoustic/install_packages.R
@@ -0,0 +1,15 @@
+
+install.packages("optparse")
+
+install.packages("seewave")
+# require: libsndfile1-dev libfftw3-dev
+# install: graphics, grDevices, stats, utils, tuneR
+
+
+install.packages("soundecology")
+# install parallel, pracma, oce, ineq, vegan, tuneR, seewave
+
+install.packages("monitoR")
+
+
+
