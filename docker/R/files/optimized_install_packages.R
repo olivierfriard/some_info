c7e88066249e7abf3fa69c8bd2dc20f2092e66db
ofriard <ofriard@occam.c3s.unito.it>
2022-07-27 17:25:38 +0200

    updated R docker

---
+++ docker/R/files/optimized_install_packages.R
@@ -0,0 +1,12 @@
+p = read.delim("/packages_list.txt", header=FALSE)
+
+for(i in seq_along(p$V1)) { if(!require(p$V1[i], character.only=TRUE)) { install.packages(p$V1[i]); }}
+
+library("devtools")
+devtools::install_github("swager/randomForestCI")
+devtools::install_github("https://github.com/Cdevenish/hardRain")
+
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+BiocManager::install(c("phyloseq"))
+
+
