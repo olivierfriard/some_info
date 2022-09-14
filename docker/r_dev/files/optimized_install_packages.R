96c48419d2ed3f886199a6ba4e368bf3331f082e
Olivier Friard <olivier.friard@unito.it>
2022-09-14 12:04:56 +0200

    some changes

--- docker/r_dev/files/optimized_install_packages.R
+++ docker/r_dev/files/optimized_install_packages.R
@@ -3,11 +3,12 @@ p = read.delim("/packages_list.txt", header=FALSE)
 for(i in seq_along(p$V1)) { if(!require(p$V1[i], character.only=TRUE)) { install.packages(p$V1[i]); }}
 
 
-#library("devtools")
-#devtools::install_github("swager/randomForestCI")
-#devtools::install_github("https://github.com/Cdevenish/hardRain")
+if(!require("devtools", character.only=TRUE)) install.packages("devtools")
+library("devtools")
+devtools::install_github("swager/randomForestCI")
+devtools::install_github("https://github.com/Cdevenish/hardRain")
 
-#if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-#BiocManager::install(c("phyloseq"))
+if (!require("BiocManager", character.only=TRUE)) install.packages("BiocManager")
+BiocManager::install(c("phyloseq"))
 
 
