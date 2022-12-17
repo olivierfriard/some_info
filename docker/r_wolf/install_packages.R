e8f3c92c35d5076e349195b1c6a4100844b06e51
Olivier Friard <olivier.friard@unito.it>
2022-12-17 22:07:50 +0100

    some work

---
+++ docker/r_wolf/install_packages.R
@@ -0,0 +1,16 @@
+install.packages('remotes')
+
+
+remotes::install_local('/Require_0.1.4.tar.gz', dependencies=T)
+
+remotes::install_local('/SpaDES.tools_1.0.0.tar.gz', dependencies=T)
+
+# NetLogoR
+remotes::install_cran('microbenchmark')
+remotes::install_local('/fastshp_0.1-2.tar.gz', dependencies=T)
+remotes::install_cran('matrixStats')
+remotes::install_cran('abind')
+
+remotes::install_local('/NetLogoR_0.3.11.tar.gz', dependencies=F)
+
+remotes::install_local('/pedantics_1.7.tar.gz', dependencies=T)
