4a88be4a49ab0432501259fb8f9da5d7a35ec7b9
Olivier Friard <olivier.friard@unito.it>
2022-12-19 14:13:22 +0100

    some changes

--- docker/r_wolf/install_packages.R
+++ docker/r_wolf/install_packages.R
@@ -11,6 +11,6 @@ remotes::install_local('/fastshp_0.1-2.tar.gz', dependencies=T)
 #remotes::install_cran('matrixStats')
 #remotes::install_cran('abind')
 
-remotes::install_local('/NetLogoR_0.3.11.tar.gz', dependencies=F)
+remotes::install_local('/NetLogoR_0.3.11.tar.gz', dependencies=T, upgrade = "never")
 
 remotes::install_local('/pedantics_1.7.tar.gz', dependencies=T)
