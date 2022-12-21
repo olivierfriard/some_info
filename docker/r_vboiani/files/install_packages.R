357dcd1c16fd98da4397dfcfaad50364a28a4caa
Olivier Friard <olivier.friard@unito.it>
2022-12-21 18:04:46 +0100

    some changes

--- docker/r_vboiani/files/install_packages.R
+++ docker/r_vboiani/files/install_packages.R
@@ -2,7 +2,11 @@ install.packages("remotes")
 
 p = read.delim("/packages_list.txt", header=FALSE)
 
+
 for (i in seq_along(p$V1)) { if(!require(p$V1[i], character.only=TRUE)) {  remotes::install_cran(p$V1[i], dependencies=T); }}
 
+remotes::install_github("swager/randomForestCI")
+
+remotes::install_bioc("phyloseq")
 
 
