82e230e5c9ee52c190c9f7c24ab7d6b046b1dcd0
Olivier Friard <olivier.friard@unito.it>
2023-01-10 17:54:04 +0100

    some work

--- docker/acoustic/install_packages.R
+++ docker/acoustic/install_packages.R
@@ -1,15 +1,20 @@
 
-install.packages("optparse")
+install.packages("remotes")
 
-install.packages("seewave")
+remotes::install_cran("optparse")
+
+remotes::install_cran("seewave")
 # require: libsndfile1-dev libfftw3-dev
 # install: graphics, grDevices, stats, utils, tuneR
 
 
-install.packages("soundecology")
+remotes::install_cran("soundecology")
 # install parallel, pracma, oce, ineq, vegan, tuneR, seewave
 
-install.packages("monitoR")
+remotes::install_cran("monitoR")
+
+remotes::install_github("https://github.com/Cdevenish/hardRain")
+
 
 
 
