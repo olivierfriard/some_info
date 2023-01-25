ace27db2dba5c8c0083c5ea08fa75939314d40bb
Olivier Friard <olivier.friard@unito.it>
2023-01-25 16:49:14 +0100

    added RDaisy

--- docker/R/files/optimized_install_packages.R
+++ docker/R/files/optimized_install_packages.R
@@ -1,10 +1,13 @@
+install.packages("remotes")
+
 p = read.delim("/packages_list.txt", header=FALSE)
 
-for(i in seq_along(p$V1)) { if(!require(p$V1[i], character.only=TRUE)) { install.packages(p$V1[i]); }}
+for(i in seq_along(p$V1)) { if(!require(p$V1[i], character.only=TRUE)) { remotes::install_cran(p$V1[i]); }}
 
-library("devtools")
-devtools::install_github("swager/randomForestCI")
-devtools::install_github("https://github.com/Cdevenish/hardRain")
+# from github
+remotes::install_github("swager/randomForestCI")
+remotes::install_github("Cdevenish/hardRain")
+remotes::install_github("nagydavid/RDaisy",subdir = "RDaisy")
 
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 BiocManager::install(c("phyloseq"))
