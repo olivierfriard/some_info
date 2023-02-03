2dd81d185ba10af07b9c144ce53dc246456c10a2
Olivier Friard <olivier.friard@unito.it>
2023-02-03 09:41:52 +0100

    some changes

--- docker/dev_r/files/install_packages.R
+++ docker/dev_r/files/install_packages.R
@@ -2,7 +2,22 @@ install.packages("remotes")
 
 p = read.delim("/packages_list.txt", header=FALSE)
 
-for (i in seq_along(p$V1)) { if(!require(p$V1[i], character.only=TRUE)) {  remotes::install_cran(p$V1[i], dependencies=T); }}
+for (i in seq_along(p$V1))
+    {
+    if(!require(p$V1[i], character.only=TRUE))
+        {
+
+	if (p$V1[i] == 'Boruta')
+            {
+            remotes::install_local('/Boruta_8.0.0.tar.gz' , build_manual=FALSE, build_vignettes=FALSE)  
+            }
+        else
+            {
+            remotes::install_cran(p$V1[i], dependencies=T, build_manual=FALSE, build_vignettes=FALSE); 
+            }
+        }
+    }
+
 
 
 
