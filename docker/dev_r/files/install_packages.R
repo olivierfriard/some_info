e31dbae0aa80e59ed3146db7bbc13f3a575df6eb
Olivier Friard <olivier.friard@unito.it>
2024-05-23 17:56:36 +0200

    some changes

--- docker/dev_r/files/install_packages.R
+++ docker/dev_r/files/install_packages.R
@@ -8,16 +8,18 @@ remotes::install_bioc("dada2", build_manual=FALSE, build_vignettes=FALSE, upgrad
 remotes::install_bioc("Biostrings", build_manual=FALSE, build_vignettes=FALSE, upgrade="always")
 remotes::install_bioc("phyloseq", build_manual=FALSE, build_vignettes=FALSE, upgrade="always")
 
-remotes::install_cran("vegan", dependencies=T, build_manual=FALSE, build_vignettes=FALSE);
+remotes::install_cran("vegan", dependencies=T, build_manual=FALSE, build_vignettes=FALSE)
 remotes::install_bioc("microbiome", build_manual=FALSE, build_vignettes=FALSE, upgrade="always")
 
 
-dplyr
-writexl
-xlsx
-readxl
-tidyr
-tibble
+remotes::install_cran("dplyr", dependencies=T, build_manual=FALSE, build_vignettes=FALSE)
+remotes::install_cran("writexl", dependencies=T, build_manual=FALSE, build_vignettes=FALSE)
+remotes::install_cran("xlsx", dependencies=T, build_manual=FALSE, build_vignettes=FALSE)
+remotes::install_cran("readxl", dependencies=T, build_manual=FALSE, build_vignettes=FALSE)
+remotes::install_cran("tidyr", dependencies=T, build_manual=FALSE, build_vignettes=FALSE)
+remotes::install_cran("tibble", dependencies=T, build_manual=FALSE, build_vignettes=FALSE)
+
+
 remotes::install_bioc("microbiomeMarker", build_manual=FALSE, build_vignettes=FALSE, upgrade="always")
 remotes::install_bioc("microbiomeutilities", build_manual=FALSE, build_vignettes=FALSE, upgrade="always")
 remotes::install_bioc("microeco", build_manual=FALSE, build_vignettes=FALSE, upgrade="always")
