261c8ea5c87aa555d1b13d334586b157137911b2
Olivier Friard <olivier.friard@unito.it>
2025-02-10 16:47:50 +0100

    some changes

--- docker/R/install_r_package_from_github.bash
+++ docker/R/install_r_package_from_github.bash
@@ -1,6 +1,8 @@
 # install R package from GitHub with remotes
 # ex: pimentel/cluster
 
-R -e "p='$1'; list.of.packages <- c(p); new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,'Package'])]; if (length(new.packages)) remotes::install_github(p, build_manual=FALSE, build_vignettes=FALSE); if (!library(p, character.only=TRUE, logical.return=T)) quit(status=10)"
+#R -e "p='$1'; list.of.packages <- c(p); new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,'Package'])]; if (length(new.packages)) remotes::install_github(p, build_manual=FALSE, build_vignettes=FALSE); if (!library(p, character.only=TRUE, logical.return=T)) quit(status=10)"
 
+
+R -e "p='$1'; list.of.packages <- c(p); new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,'Package'])]; if (length(new.packages)) remotes::install_github(p, build_manual=FALSE, build_vignettes=FALSE);"
 
